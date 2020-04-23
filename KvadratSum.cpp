4993b3b84c2ad2e9597753a61e8581e6f129101a
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-23 20:21:57 +0300

    Add files via upload

---
+++ KvadratSum.cpp
@@ -0,0 +1,26 @@
+﻿// KvadratSum.cpp
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>null");
+    int n, s = 0;
+    cout << "Upper summ border: ";
+    cin >> n;
+    if (n > 0){
+        while (n) {
+            s += n * n;
+            n--;
+        }
+        cout << "Kvadrat summ: " << s << endl;
+    }
+    else {
+        cout << "Wrong number" << endl;
+    }
+    system("pause>nul");
+    return 0;
+}
